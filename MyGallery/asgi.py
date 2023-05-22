1ffae61a9a7ac30258e2b478c05ebe6e71185f65
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-22 18:54:01 +0900

    first commit

---
+++ MyGallery/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MyGallery project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyGallery.settings")
+
+application = get_asgi_application()

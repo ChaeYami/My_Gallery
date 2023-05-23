947098a80b4f2db94de550315d6430fd01baae14
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-23 17:10:29 +0900

    setting MySQL

--- MyGallery/settings.py
+++ MyGallery/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import os
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,7 @@ WSGI_APPLICATION = "MyGallery.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation

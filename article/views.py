fb0666a9bdc4c7424dbe0cb8b69b921dc73ee7b9
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-23 14:48:01 +0900

    comments 위치수정

--- article/views.py
+++ article/views.py
@@ -9,34 +9,6 @@ from .serializers import (
     CommentsSerializer,
     CommentsCreateSerializer)
 
-class CommentsView(APIView):
-    # ===================== 댓글 목록 보기 =========================
-    def get(self, article_id):
-        article = get_object_or_404(Articles, id=article_id)
-        comments = article.comments.all()
-        serializer = CommentsSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    # ======================== 댓글 작성 ============================
-    def post(self, request, article_id):
-        serializer = CommentsCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user, article_id=article_id)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-    # ======================== 댓글 삭제 ============================
-    def delete(self, request, comment_id):
-        comment = get_object_or_404(Comments, id=comment_id)
-        if request.user == comment.user:
-            comment.delete()
-            return Response({"message": "삭제완료!"}, status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response({"message": "댓글 작성자만 삭제 가능."}, status=status.HTTP_403_FORBIDDEN)
-
-
-
 from article.serializers import (
     ArticleSerializer,
     ArticleListSerializer,
@@ -87,3 +59,31 @@ class ArticleDetailView(APIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+
+class CommentsView(APIView):
+    # ===================== 댓글 목록 보기 =========================
+    def get(self, article_id):
+        article = get_object_or_404(Article, id=article_id)
+        comments = article.comments.all()
+        serializer = CommentsSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # ======================== 댓글 작성 ============================
+    def post(self, request, article_id):
+        serializer = CommentsCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, article_id=article_id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # ======================== 댓글 삭제 ============================
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comments, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response({"message": "삭제완료!"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message": "댓글 작성자만 삭제 가능."}, status=status.HTTP_403_FORBIDDEN)
+
+

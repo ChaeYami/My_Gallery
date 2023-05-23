557c088f47a2dba582f7d5ec024cb199e2dea94b
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-23 16:57:17 +0900

    merge

--- article/models.py
+++ article/models.py
@@ -26,7 +26,7 @@ class Article(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     title = models.CharField(max_length=50, verbose_name="제목")
     content = models.TextField(verbose_name="내용")
-    hearts = models.ManyToManyField(Users,blank = True, related_name= 'hearts')
+    hearts = models.ManyToManyField(User,blank = True, related_name= 'hearts')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성시간")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정시간")
 

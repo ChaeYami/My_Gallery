597180a63f25a004c643e3b16fdc1cfd3ef7d4fc
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-23 11:59:12 +0900

    modify url

--- user/urls.py
+++ user/urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
     
     path('<int:user_id>/', views.ProfileView.as_view(), name="profile_view"), # /user/<int:user_id>/
 
+    path("password/email/", views.PasswordResetView.as_view(), name="password_reset"),  # 비밀번호 찾기 (이메일 보내기)
+    path("auth/password/reset/b'<str:uidb64>'/<str:token>/", views.PasswordTokenCheckView.as_view(), name="password_reset_confirm"),  # 비밀번호 재설정 토큰 확인
+    path("password/reset/", views.SetNewPasswordView.as_view(), name="password_reset_confirm"),  # 비밀번호 재설정
+    
     # path("password/email/", views.PasswordResetView.as_view(), name="password_reset"),  # /user/password/email/ 비밀번호 찾기 (이메일 보내기)
     # path("password/check/", views.PasswordTokenCheckView.as_view(), name="password_reset_confirm"),  # /user/password/check/ 비밀번호 재설정 토큰 확인
     # path("password/reset/", views.SetNewPasswordView.as_view(), name="password_reset_confirm"),  # /user/password/reset/ 비밀번호 재설정

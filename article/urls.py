493cf0991985ddc8a4e307d0b8c7cbd69a4d59a2
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-24 20:26:53 +0900

    좋아요 url 추가

--- article/urls.py
+++ article/urls.py
@@ -4,15 +4,12 @@ from article import views
 
 urlpatterns = [
     path("", views.ArticleView.as_view(), name="article_view"),
-    path(
-        "<int:article_id>/",
-        views.ArticleDetailView.as_view(),
-        name="article_detail_view",
-    ),
+    path("<int:article_id>/", views.ArticleDetailView.as_view(),name="article_detail_view",),
+    
+    path('<int:article_id>/hearts/', views.ArticleHeartsView.as_view(),name='Hearts_view'),  # 좋아요 기능
+    
     path("<int:article_id>/comment/", views.CommentView.as_view(), name="comment_view"),
-    path(
-        "comment/<int:comment_id>/", views.CommentView.as_view(), name="comment_delete"
-    ),
+    path("comment/<int:comment_id>/", views.CommentView.as_view(), name="comment_delete"),
     
     path('list/<int:user_id>/', views.ArticleListView.as_view(), name='article_list'),  # /article/<int:article_id>/list
 ]

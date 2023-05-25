3d64113758510c3a2d30185b80dcf9618c474255
hyunah <hil02165@naver.com>
2023-05-26 04:57:43 +0900

    add user_tests.py test_signup

--- user/tests.py
+++ user/tests.py
@@ -1,19 +1,22 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
+from rest_framework.test import APITestCase, APIClient, override_settings
+from django.test import override_settings
 from user.models import User
+from base64 import urlsafe_b64encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_bytes, smart_bytes
 
 # Create your tests here.
-class UserSingUpTest(APITestCase):
-    def test_registration(self):
+class SignupViewTest(APITestCase):
+    # 회원가입 테스트 코드
+    def test_signup(self):
         url = reverse("sign_up_view")
-        user_data = {
-            "email" : "sngffd@naver.com",
-            "account" : "asdgwe",
-            "nickname" : "test1",
-            "password" : "G1843514dadg23!4"
-            }
-        response = self.client.post(url, user_data)
+        data = {"email" : "sdgasdf@naver.com",
+                "account" : "SHGDF",
+                "nickname" : "test1",
+                "password" : "G1843514dadg23!4"
+                }
+        response = self.client.post(url, data)
         print(response.data)
         self.assertEqual(response.status_code, 201)
 

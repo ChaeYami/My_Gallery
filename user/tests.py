7185a79a22514bd1f77eade05235cac76448664d
hyunah <hil02165@naver.com>
2023-05-26 05:01:35 +0900

    add user_tests.py test_email_verification

--- user/tests.py
+++ user/tests.py
@@ -21,3 +21,27 @@ class SignupViewTest(APITestCase):
         self.assertEqual(response.status_code, 201)
 
 
+class VerifyEmailViewTest(APITestCase):
+    # setUp 함수 : 테스트 코드 실행 전에 실행하겠다.(유저 생성)
+    def setUp(self):            
+        self.user = User.objects.create_user(email='sdgasdf@naver.com',
+                                             account='admin',
+                                             nickname='admin',
+                                             password='G1843514dadg23@')
+
+    # 이메일 인증 테스트 코드
+    @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend") # 이메일을 실제로 전송하지 않고 로컬 메모리에 저장하는 백엔드
+    def test_email_verification(self):
+        token = PasswordResetTokenGenerator().make_token(self.user)
+        uid = urlsafe_b64encode(force_bytes(self.user.pk))
+        
+        verification_link = f'http://localhost:8000/user/verify-email/{uid}/{token}/'  # 확인 링크를 가정합니다.
+        response = self.client.get(verification_link)
+
+        # self.assertEqual(response.status_code, 200)  # return Response 사용할 경우 : 확인 링크를 클릭하여 이메일 주소를 확인합니다.
+        self.assertEqual(response.status_code, 302) # 302 리다이렉트 상태 코드
+        self.assertRedirects(response, "http://127.0.0.1:5500/user/login.html", fetch_redirect_response=False)
+        # fetch_redirect_response=False 외부 서비스나 다른 서버에서 제공될 경우 사용되며 원격 URL을 가져오지 않고, 상태코드와 리다이렉트 대상 URL에 집중하여 검증진행.
+
+        self.user.refresh_from_db()  # 사용자 데이터 다시 불러오기
+        self.assertTrue(self.user.is_active)  # 사용자가 활성화 상태인지 확인

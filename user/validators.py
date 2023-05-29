02397a27534697ccaace848c363d24fd143d0066
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-29 10:17:18 +0900

    불필요한 주석 제거

--- user/validators.py
+++ user/validators.py
@@ -1,9 +1,6 @@
 import re
 
-# 8자 이상의 영문 대소문자와 숫자, 특수문자를 포함
 def password_validator(password):
-    # password_regex = '^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,}$'
-    
     # 8자 이상의 영문 대소문자와 숫자, 특수문자 포함
     password_regex = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
     
@@ -11,7 +8,6 @@ def password_validator(password):
         return True
     return False
 
-# 인자값으로 들어온 문자열의 문자중 연속해서 3개 이상의 문자가 같은 패턴인지 검사합니다.
 def password_pattern(password):
     password_pattern = r"(.)\1+\1"
     
